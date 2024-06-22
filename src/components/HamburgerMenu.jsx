bcf1200347f6ab5d1268bbd32a38b5738b304306
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-06-22 15:02:23 -0400

    dropdown lists

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-import dropDown from "../assets/dropDown.svg"
+import React, { useState } from "react";
+import dropDown from "../assets/dropDown.svg";
 
 const Menu = () => {
-    
-    return (
-        <div className="flex invisible md:visible items-start justify-center absolute w-1/12 h-1/6">
-        <div className="w-1/2 lg:w-1/3 p-2 lg:p-1 mt-2 h-1/4 flex justify-center items-center bg-yellow-100 border shadow-md rounded-md  absolute">
-        <div>
-         <img src={dropDown} alt="dropdown" className="w-full h-full object-contain"/>
-        </div>
-        </div>
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Menu
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex flex-col invisible md:visible items-start justify-center bg-blue-200 absolute w-1/12 h-1/4">
+      <div className="w-1/2 xl:w-1/4 p-1 lg:p-1 top-2 h-1/5 flex relative left-6  justify-center items-center bg-yellow-100 border shadow-md rounded-md  absolute">
+        <div className="" onClick={toggleDropdown}>
+          <img
+            src={dropDown}
+            alt="dropdown"
+            className="w-full h-full object-contain"
+          />
+        </div>
+      </div>
+      <div className="w-full h-full">
+      {isOpen && (
+        <div className="bg-green-100 w-full h-full translate-x-6 transform translate-y-2 absolute rounded-lg relative">
+          <ul className="divide-y divide-red-400  flex flex-col justify-center h-full items-center">
+            <li>Ji</li>
+            <li>Ji</li>
+            <li>Ji</li>
+            <li>Ji</li>
+            <li>Ji</li>
+          </ul>
+        </div>
+      )}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
