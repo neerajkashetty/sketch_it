261d1fe1026e8987618d293d3404d247d77b80b4
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-12-27 22:14:30 -0500

    Start the rohiths portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import Toolbar from "./components/Toolbar"
-import Sketch from "./components/Sketch"
-import Menu from "./components/HamburgerMenu"
-import Share from "./components/Share"
-import React, {useState} from "react"
+import React from "react";
 
 function App() {
-  const [colorpicker, setIsColorPicker] = useState('')
-
   return (
     <>
-    <div id="start" className="h-screen w-screen">
-      <Share/>
-        <Menu/>
-       <Toolbar setIsColorPicker={setIsColorPicker}/>   
-       <Sketch colorpicker={colorpicker}/>
-    </div>
+      <div id="start" className="h-screen w-screen">
+        Fuck you
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
